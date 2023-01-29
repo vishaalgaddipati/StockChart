6eaeb15aceae4f9d92ab4bb4f35da741cac04991
vishaalgaddipati <102570970+vishaalgaddipati@users.noreply.github.com>
2023-01-28 18:33:15 -0800

    Added csv splits functionality

--- StockChart.java
+++ StockChart.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,39 +11,76 @@ import java.util.Scanner;
 public class StockChart {
     
     // Variables
+    static String[][] stockData;
     //String downloadURL = "https://query1.finance.yahoo.com/v7/finance/download/GOOG?period1=1643320177&period2=1674856177&interval=1d&events=history&includeAdjustedClose=true";
     
 
     // Constructor
     public StockChart(String name) {
-        
+        return;
     }
 
     // Functions
-    public void setName() {
-        return;
+    static float findMin(String[][] stockData, int col) {
+        float min = 0;
+
+        return min;
     }
 
-    public void displayData() {
-        return;
+    static float findMax();
+
+
+    static String[][] getStockData() {
+        return stockData;
+    }
+
+    static long getFileLength(File file) throws FileNotFoundException {
+        Scanner in = new Scanner(file);
+        long lines = 0;
+        while(in.hasNext()) {
+            in.nextLine();
+            lines++;
+        }
+        return lines;
+    }
+
+    static String[][] readCSV(File file, long length) throws FileNotFoundException {
+        //Split the CSV file into an array using .split()
+        Scanner in = new Scanner(file);
+        String[] header = in.nextLine().split(","); // nextLine() skips white space
+        int size = header.length;
+        stockData = new String[(int)length][size];
+        int i = 0;
+        while(in.hasNext()) {     // loop scanning csv line by line
+            stockData[i] = in.nextLine().split(","); // setting row in stockData to csv array
+            i++;
+        }
+        in.close();
+        return stockData;
     }
 
     // main class
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // reading in the stock name 
         System.out.print("Enter stock: ");
         String stockName = in.nextLine();
 
         //File file1 = new File("Data/GOOG.csv");
-        File file1 = new File("Data/" + stockName + ".csv");
+        File file1 = new File("Data/" + stockName + ".csv");   // reading csv file
         FileInputStream fileRead = new FileInputStream(file1);
-        long length = file1.length();
+        long length = getFileLength(file1);
         // for (int i = 0; i < length; i++) {
         //     System.out.print((char)fileRead.read());
         // }
+        String[][] currStockData = getStockData();
+        currStockData = readCSV(file1, length);
+        for (int i = 0; i < length - 1; i++) {
+                System.out.print(currStockData[i][0] + " " + currStockData[i][4]);
+                System.out.println();
+        }
         fileRead.close();
 
-        JFrame frame = new JFrame(stockName);
+        JFrame frame = new JFrame(stockName);                     // creating frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Graph testGraph = new Graph(fileRead, length, stockName);
         //JLabel emptyLabel = new JLabel();
