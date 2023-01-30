b058dc7c39e53feba1aa98817b67aaa61956f369
vishaalgaddipati <102570970+vishaalgaddipati@users.noreply.github.com>
2023-01-29 20:54:23 -0800

    Fixed offsets

--- Graph.java
+++ Graph.java
@@ -33,22 +33,22 @@ public class Graph extends JPanel {
     public void paintComponent(Graphics g) {
         Graphics2D gr = (Graphics2D) g;
         Dimension windowSize = getSize();
-        float ySlope =  0 - windowSize.height/(int)(max-min);
+        int yOffset = 100;
+        float ySlope =  (yOffset - windowSize.height)/(int)(max-min);
         int prevPoint = ((int)(Float.parseFloat(stockData[0][col])));
-        System.out.println(prevPoint + " " + windowSize.height + " " + ySlope);
-        prevPoint = windowSize.height + (int)((prevPoint-min)*ySlope);
+        int xOffset = 50;
+        prevPoint = windowSize.height - 100 + (int)((prevPoint-min)*ySlope);
         gr.drawString(stockName, 0, 10);
         gr.drawString("Min: " + min, 0, 20);
         gr.drawString("Max: " + max, 0, 30);
-        System.out.println(prevPoint);
         for (int i = 1; i < length - 1; i++) {
             //int currPoint = (int)(windowSize.height-(Float.parseFloat(stockData[i][col])*(windowSize.height-100)/(max-min)));
             int currPoint = (int)(Float.parseFloat(stockData[i][col]));
-            currPoint = windowSize.height + (int)((currPoint-min)*ySlope);
+            currPoint = windowSize.height - 100 + (int)((currPoint-min)*ySlope);
             gr.setStroke(new BasicStroke(1));
             gr.setColor(Color.BLUE);
-            int newX1 = (windowSize.width*(i))/(int)length;
-            int newX2 = (windowSize.width*(i + 1))/(int)length;
+            int newX1 = ((windowSize.width - xOffset)*(i))/(int)length + xOffset;
+            int newX2 = ((windowSize.width - xOffset)*(i + 1))/(int)length + xOffset;
             int newY1 = prevPoint;
             int newY2 = currPoint;
             gr.drawLine(newX1, newY1, newX2, newY2);
